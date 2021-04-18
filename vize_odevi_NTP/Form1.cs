89c4e75450c3d447e4d0b756576224d2606fbdcc
FatihBicici <f_bicici@outlook.com>
2021-04-18 17:15:07 +0300

    Verilerin çekileceği ekranı datagridview,textbox ve 3 tane sutun ekleyerek tasarladım.

--- vize_odevi_NTP/Form1.cs
+++ vize_odevi_NTP/Form1.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace vize_odevi_NTP
 {
     public partial class Form1 : Form
@@ -15,6 +14,21 @@ namespace vize_odevi_NTP
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
